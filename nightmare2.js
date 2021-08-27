6d2eef3a4fdaea28b33c2a8167ff0773af6d04b7
Morgan Metz <68765538+Morgandri1@users.noreply.github.com>
2021-08-27 16:05:11 -0400

    Add files via upload

---
+++ nightmare2.js
@@ -0,0 +1,12 @@
+var http = require('http');
+
+http.createServer(function (req, res) {
+  var body = "";
+  req.on('data', function (chunk) {
+    body += chunk;
+  });
+  req.on('end', function () {
+    console.log('POSTed: ' + body);
+    res.writeHead(200);
+  });
+}).listen(8080);
